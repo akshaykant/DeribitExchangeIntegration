ecdee01b79f81cfec61a8d1302de6eb36129ebb0
akshaykant <akshaykant1708@gmail.com>
2021-07-18 10:13:28 +0100

    Initial commit

---
+++ src/main/java/com/akshaykant/coppertest/CoppertestApplication.java
@@ -0,0 +1,13 @@
+package com.akshaykant.coppertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoppertestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoppertestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshaykant/coppertest/CoppertestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshaykant.coppertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoppertestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
